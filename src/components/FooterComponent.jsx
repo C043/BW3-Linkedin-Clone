dd39e72b20305fd00fb71bf6a1d999f7440f966f
Mario Fragnito <mariofragnitoph@gmail.com>
2024-07-15 17:53:17 +0200

    WIP

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,17 @@
+import { Col, Row } from "react-bootstrap";
+
+const FooterComponent = () => {
+  return (
+    <Row>
+      <Col>
+        <a href="">Informazioni</a>
+        <a href="">Linee guida della community</a>
+        <a href="">Privacy e condizioni</a>
+        <a href="">Sales Solutions</a>
+        <a href="">Centro sicurezza</a>
+      </Col>
+    </Row>
+  );
+};
+
+export default FooterComponent;
