643017044c832cc1d297911f1920679af5738270
MelRecalde <melquisedicrecalde@gmail.com>
2024-07-15 15:41:59 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import ProfilePage from "./components/ProfilePage";
 import { Container } from "react-bootstrap";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
+    <>
+    <NavBar />
     <Container>
       <ProfilePage />
     </Container>
+    </>
   );
 }
 
--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserAction } from "../redux/actions";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 
 const ProfilePage = () => {
   const dispatch = useDispatch();
