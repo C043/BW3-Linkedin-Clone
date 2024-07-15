4b1a08be77c878b53a7a4fe42fda5169911e3af6
Mario Fragnito <mariofragnitoph@gmail.com>
2024-07-15 11:56:46 +0200

    Adds cover pic

---
+++ src/components/CoverPic.jsx
@@ -0,0 +1,13 @@
+import { Image } from "react-bootstrap";
+
+const CoverPic = () => {
+  return (
+    <Image
+      fluid
+      src="https://media.licdn.com/dms/image/C5616AQEHJvVztoiMuw/profile-displaybackgroundimage-shrink_350_1400/0/1662109726252?e=1726704000&v=beta&t=7_5TijBcSl-DWmQvI_il-W0fWtbzasSZLpy2v4ESVoA"
+      alt="cover-image"
+    />
+  );
+};
+
+export default CoverPic;
--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getUserAction } from "../redux/actions";
 import { Col, Container, Row } from "react-bootstrap";
 import ProfilePic from "./ProfilePic";
+import CoverPic from "./CoverPic";
 
 const ProfilePage = () => {
   const dispatch = useDispatch();
@@ -15,6 +16,7 @@ const ProfilePage = () => {
   return (
     <Row>
       <Col xs="12" md="9">
+        <CoverPic />
         <ProfilePic />
       </Col>
       <Col md="3" className="d-none d-md-block"></Col>
