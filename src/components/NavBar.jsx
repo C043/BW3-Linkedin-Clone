edab5a66ad90a0ee3669007c38459f315f1cb97b
Mario Fragnito <mariofragnitoph@gmail.com>
2024-07-17 19:01:00 +0200

    Fixed some issues

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -17,7 +17,7 @@ const NavBar = () => {
 
           <Form>
             <Form.Group className="border d-flex align-items-center ps-2 rounded-3" controlId="search">
-              <FormLabel>
+              <FormLabel type="button">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="20"
@@ -29,7 +29,7 @@ const NavBar = () => {
                   <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
                 </svg>
               </FormLabel>
-              <Form.Control type="text" placeholder="Cerca" className="mr-sm-2 border-0 main-button" id="search" />
+              <Form.Control type="text" placeholder="Cerca" className="mr-sm-2 border-0 search-input" />
             </Form.Group>
           </Form>
 
