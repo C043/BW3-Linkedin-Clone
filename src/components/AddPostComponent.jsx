1d8e76d513f3badbd8997b44a134600d99148089
Mario Fragnito <mariofragnitoph@gmail.com>
2024-07-17 12:29:02 +0200

    Update AddPostComponent.jsx

--- src/components/AddPostComponent.jsx
+++ src/components/AddPostComponent.jsx
@@ -9,19 +9,19 @@ const AddPostComponent = () => {
         <input type="text" className="w-100 rounded-pill border px-3 py-3" placeholder="Crea un post" />
       </div>
       <div className="d-flex gap-3 justify-content-center my-3">
-        <div className="d-flex gap-2 align-items-center">
+        <div type="button" className="d-flex gap-2 align-items-center">
           <CardImage className="fs-4 text-primary" />
           <p className="m-0">
             <b>Contenuti multimediali</b>
           </p>
         </div>
-        <div className="d-flex gap-2 align-items-center">
+        <div type="button" className="d-flex gap-2 align-items-center">
           <Calendar className="fs-4 text-warning" />
           <p className="m-0">
             <b>Evento</b>
           </p>
         </div>
-        <div className="d-flex gap-2 align-items-center">
+        <div type="button" className="d-flex gap-2 align-items-center">
           <TextIndentLeft className="fs-4 text-danger" />
           <p className="m-0">
             <b>Scrivi un articolo</b>
