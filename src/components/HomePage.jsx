d0b291451cc815ecc6a223fb5ccafadd149a864c
Mario Fragnito <mariofragnitoph@gmail.com>
2024-07-17 09:49:44 +0200

    Adds basic structure to home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,13 @@
+import { Col, Row } from "react-bootstrap";
+
 const HomePage = () => {
-  return;
+  return (
+    <Row>
+      <Col xs="12" md="2"></Col>
+      <Col xs="12" md="10" lg="5" xl="7"></Col>
+      <Col xs="12" lg="5" xl="3" className="d-none d-lg-block"></Col>
+    </Row>
+  );
 };
 
 export default HomePage;
