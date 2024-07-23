6df6c05c380ebbffa8ec8cd9755640a305b1a817
Mario Fragnito <mariofragnitoph@gmail.com>
2024-07-23 18:12:15 +0200

    Fixed some issues

--- src/components/BigHeroLoadingComponent.jsx
+++ src/components/BigHeroLoadingComponent.jsx
@@ -3,11 +3,10 @@ import Skeleton from "react-loading-skeleton";
 const BigHeroLoadingComponent = () => {
   return (
     <div className="my-2 py-2">
-      <div className="d-flex gap-2 mb-2 ">
-        <Skeleton circle style={{ width: "125px", height: "125px" }} />
-        <Skeleton style={{ height: "50px" }} />
-      </div>
-      <Skeleton count={5} />
+      <Skeleton circle style={{ width: "125px", height: "125px" }} className="mb-2" />
+      <p>
+        <Skeleton count={5} />
+      </p>
     </div>
   );
 };
--- src/components/HeroLoadingComponent.jsx
+++ src/components/HeroLoadingComponent.jsx
@@ -3,11 +3,10 @@ import Skeleton from "react-loading-skeleton";
 const HeroLoadingComponent = () => {
   return (
     <div className="my-2 py-2">
-      <div className="d-flex gap-2 mb-2 ">
-        <Skeleton circle style={{ width: "50px", height: "50px" }} />
-        <Skeleton style={{ height: "50px" }} />
-      </div>
-      <Skeleton count={3} />
+      <Skeleton circle style={{ width: "50px", height: "50px" }} className="mb-2" />
+      <p>
+        <Skeleton count={3} />
+      </p>
     </div>
   );
 };
